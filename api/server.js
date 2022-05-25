da31a4f652b599a46842440df71bee94000c5d73
Slan <snchong.slan@gmail.com>
2022-05-25 15:43:04 +0800

    mongodb url

--- api/server.js
+++ api/server.js
@@ -9,10 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://slan1024:passwordslan1024@mern.pw4avrz.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connected to DB"))
   .catch(console.error);
 
